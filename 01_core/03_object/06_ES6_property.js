33ed4430466277432e344b92dff0150e19ddd7e2
nicdkim <zzsehdrb@naver.com>
2024-09-10 16:24:32 +0900

    js

--- 01_core/03_object/06_ES6_property.js
+++ 01_core/03_object/06_ES6_property.js
@@ -8,3 +8,35 @@ let product = {
     price : price
 };
 console.log(product);
+
+console.log("-----------------------------");
+
+// ES6 단축 문법
+let product2 = {id,price};
+console.log(product2);
+
+console.log("-----------------------------");
+
+// 메소드 단축
+
+let dog = {
+    name : "흰둥이",
+    eat : function(food) {
+        console.log(`${this.name}는 ${food}를 맛있게 먹습니다.`);
+    }
+};
+
+dog.eat("고구마");
+
+console.log("-----------------------------");
+
+let dog2 = {
+    name : "뽀삐",
+    eat(food) {
+        console.log(`${this.name}는 ${food}를 맛있게 먹습니다.`);
+    }
+};
+dog2.eat("감자");
+
+console.log("-----------------------------");
+
